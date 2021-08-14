c40a60f489756a461100da9bfb46e8a4e132ad06
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-08-14 14:19:48 -0300

    First Commit

---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Temperatura {
+	
+	public static void main(String[] args) {
+		
+		// (Fº - 32) x 5/9 = Cº
+		final double FATOR = 5.0 / 9;
+		final int AJUSTE = 32;
+		
+		int tempF = 86;
+		double celsius = (tempF - AJUSTE) * FATOR;
+		System.out.println("O resultado é " + celsius + "°C");
+		
+		tempF = 0;
+		celsius = (tempF - AJUSTE) * FATOR;
+		System.out.println("O resultado é " + celsius + "°C");
+		
+	}
+}
