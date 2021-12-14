ab010b47165af354ddb1b8f2e5a1100dff411a64
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-12-13 22:27:25 -0300

    Capítulo de Generics

---
+++ exercicios/src/generics/Par.java
@@ -0,0 +1,34 @@
+package generics;
+
+public class Par<C, V> {
+
+	private C chave;
+	private V valor;
+
+	public Par() {
+
+	}
+
+	public Par(C chave, V valor) {
+		super();
+		this.chave = chave;
+		this.valor = valor;
+	}
+
+	public C getChave() {
+		return chave;
+	}
+
+	public void setChave(C chave) {
+		this.chave = chave;
+	}
+
+	public V getValor() {
+		return valor;
+	}
+
+	public void setValor(V valor) {
+		this.valor = valor;
+	}
+
+}
